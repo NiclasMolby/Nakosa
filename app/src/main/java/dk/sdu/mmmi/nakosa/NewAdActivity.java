411f709bef07acd6a00ee5a667fdadc31c87c925
Niclas Mølby <niclas@molby.net>
2018-02-22 23:26:55 +0100

    Picture is saved, and big thumbnail is shown

--- app/src/main/java/dk/sdu/mmmi/nakosa/NewAdActivity.java
+++ app/src/main/java/dk/sdu/mmmi/nakosa/NewAdActivity.java
@@ -2,17 +2,27 @@ package dk.sdu.mmmi.nakosa;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class NewAdActivity extends AppCompatActivity {
 
     private String picturePath;
+    private final int CAMERA_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +34,57 @@ public class NewAdActivity extends AppCompatActivity {
 
     public void useCamera(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, 1);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_CODE);
+            }
         }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
+        if (requestCode == CAMERA_CODE && resultCode == RESULT_OK) {
+            /*Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             Log.d("Camera", "Width: "+imageBitmap.getWidth());
             Log.d("Camera", "Height: " + imageBitmap.getHeight());
             ImageView image = findViewById(R.id.imageFromCamera);
             image.setImageBitmap(imageBitmap);
+            image.setVisibility(View.VISIBLE);*/
+            Log.d("Photo", "Photo taken - "+ picturePath);
+            Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+            ImageView image = findViewById(R.id.imageFromCamera);
+            image.setImageBitmap(bitmap);
             image.setVisibility(View.VISIBLE);
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        picturePath = image.getAbsolutePath();
+        return image;
     }
 
     @Override
