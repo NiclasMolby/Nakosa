366ed172be9c2602351713ddb09891e506ff31f3
Unknown <lascaritheman@gmail.com>
2018-02-26 18:17:12 +0100

    Implementeret Facebook login
    og det virker....

---
+++ app/src/main/java/dk/sdu/mmmi/nakosa/MainActivity.java
@@ -0,0 +1,61 @@
+package dk.sdu.mmmi.nakosa;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        ((TextView) findViewById(R.id.textView)).setText("LOGGED IN FUCKING VIRKEDE!");
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        ((TextView) findViewById(R.id.textView)).setText("Logged in cancelled...!");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        ((TextView) findViewById(R.id.textView)).setText("Logged in fejlede :(");
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
