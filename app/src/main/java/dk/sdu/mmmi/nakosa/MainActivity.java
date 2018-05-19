19ececfe3861063b7b596351b9b1315884e83583
Unknown <lascaritheman@gmail.com>
2018-05-19 19:31:40 +0200

    Small fixes, should work now

--- app/src/main/java/dk/sdu/mmmi/nakosa/MainActivity.java
+++ app/src/main/java/dk/sdu/mmmi/nakosa/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
@@ -88,8 +89,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.bypass)
-    public void bypass(){
-        Log.d("Something", "Im into bypass");
+    public void bypass(View view){
         userData.setFirstName("TEST");
         userData.setLastName("ACCOUNT");
         changeView();
